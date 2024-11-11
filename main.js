35309891e6dceedf893538b780c53c2e3625d66c
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-11-11 19:39:40 +0100

    Scroll header added

--- main.js
+++ main.js
@@ -29,6 +29,16 @@ const scrollUp = () => {
 window.addEventListener("scroll", scrollUp)
 
 /*~~~~~~~~~~~~~~~~~CHANGE BACKROUND HEADER~~~~~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+  const header = document.getElementById("navbar");
+
+  if (this.scrollY >= 50) {
+    header.classList.add("border-b", "border-yellow-500");
+  } else {
+    header.classList.remove("border-b", "border-yellow-500");
+  }
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~~~SWIPER~~~~~~~~~~~~~~~~~~~*/
 const swiper = new Swiper(".swiper", {
